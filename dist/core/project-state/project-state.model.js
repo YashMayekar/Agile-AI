3167e8d4f0ee59d19e6faf3847c76909b1370bd7
Yash Mayekar <yashvmayekar@gmail.com>
2026-05-28 22:00:52 +0530

    Docker Compose file updated to fix the OLLAMA endpoint issue

--- dist/core/project-state/project-state.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isDirectory = isDirectory;
-/**
- * Type guard to check if a node is a directory.
- */
-function isDirectory(node) {
-    return node.children !== undefined;
-}
--- dist/llm/llm-adapter.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * LLM Adapter Interface
- *
- * All LLM providers MUST implement this interface.
- * This ensures the rest of the system remains provider-agnostic.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
